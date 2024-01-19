2003175ada1b0deec061c55545d209498862f07f
Caylin Dewey <caylindewey42@gmail.com>
2024-01-19 14:40:25 +0000

    Updated nav bar to link to about and contact pages

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-<!-- jQuery -->
-<
-script src = "https://code.jquery.com/jquery-3.3.1.slim.min.js" > < /script> <
-    !--Popper.js -->
-    <
-    script src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" > < /script> <
-    !--Bootstrap JavaScript-- >
-    <
-    script src = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" > < /script>
+// nav bar
+
+$(document).ready(function () {
+    // Add an event listener for the navbar-toggler button
+    $('.navbar-toggler').click(function () {
+        // Toggle the navigation menu when the toggler is clicked
+        $('.navbar-collapse').toggleClass('show');
+    });
+});
