af961730d9f3b1ed4594fd6a62b7606204bab213
Kayla S <kaylaesmith1@gmail.com>
2024-01-19 20:05:36 +0000

    Style buttons, needs, luxury sections. Add logo, favicon. Implement color scheme. Fix hamburger toggle.

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// nav bar
-
-$(document).ready(function () {
-    // Add an event listener for the navbar-toggler button
-    $('.navbar-toggler').click(function () {
-        // Toggle the navigation menu when the toggler is clicked
-        $('.navbar-collapse').toggleClass('show');
-    });
-});
